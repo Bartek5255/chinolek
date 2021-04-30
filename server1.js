f995397dee76d93119e5aef994dba6831d3a22ba
Bartek5255 <Bartek5255@hotmail.com>
2021-04-30 21:30:59 +0200

    add package.json and server port change

--- server1.js
+++ server1.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 var gry = [];
 var session = require('express-session')
 var currentTime = 0
